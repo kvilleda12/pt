8e74a53ca8839e15317eae82e4ff367c3a3a1d3c
Adam Soliman <aes10130@nyu.edu>
2025-07-02 19:45:33 -0400

    Files added for the ui initialization. I made the home page, about page, and navbar.

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/app/ui-components/navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PTI",
+  description: "PTI is an AI powered bot that helps you with physical therapy in many ways. It gives you excersices you can do after telling it what body part you have problems with, guides you through excersices and analyzes them if you are doing them correctly. It also helps track a variety of data that can be used by others to hopefully revolutionize a sector or sectors fo the body.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
