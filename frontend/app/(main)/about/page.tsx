c762c1620ab887a547031a38482cb85610ef0f21
Adam Soliman <aes10130@nyu.edu>
2025-07-14 18:55:53 -0400

    Animations added.

--- frontend/app/(main)/about/page.tsx
+++ frontend/app/(main)/about/page.tsx
@@ -1,4 +1,5 @@
 import styles from "../../page.module.css";
+import Image from "next/image";
 
 // About Page
 export default function About() {
@@ -7,6 +8,17 @@ export default function About() {
       <h2 className={styles.title}>About PTI</h2>
       <p className={styles.aboutDescription}>
         PTI is an AI-powered assistant designed to support you through every step of your physical therapy journey. After you describe the area of your body you're experiencing issues with, PTI suggests targeted exercises, guides you through them, and analyzes your form to ensure you're performing them correctly. It also tracks a variety of physical data that can be used to drive insights — potentially revolutionizing how specific areas of the body are treated across the field.      </p>
+      <h2 className={styles.sectionHeader}>Meet Our Founders</h2>
+      <div className={styles.founderProfile}>
+        <p className={styles.founderName}>Kevin Villeda de Leon</p>
+        <p className={styles.aboutDescription}>Kevin is a Data Science student at BU...</p>
+        <Image src="/kevin.jpeg" alt="Kevin Villeda de Leon" width={100} height={100} className={styles.founderImage}/>
+      </div>
+      <div className={styles.founderProfile}>
+        <p className={styles.founderName}>Adam Soliman</p>
+        <p className={styles.aboutDescription}>Adam is a Computer Science student at NYU...</p>
+        <Image src="/adam.jpg" alt="Adam Soliman" width={100} height={100} className={styles.founderImage}/>
+      </div>
     </div>
   );
 }
--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,10 +1,30 @@
+'use client';
 import Link from "next/link";
 import styles from "../page.module.css";
+import SplitText from "@/app/ui-components/reactbits/SplitText.js";
+import StarBorder from "@/app/ui-components/reactbits/StarBorder.js";
 
 export default function Home() {
+  const handleAnimationComplete = () => {
+    console.log('Title animated!');
+  };
+
   return (
     <div className={styles.page}>
-      <h2 className={styles.title}>PTI</h2>
+      <SplitText
+        text="Welcome to PTI"
+        className={styles.title}
+        delay={10}
+        duration={2}
+        ease="elastic.out(1, 0.3)"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin="-100px"
+        textAlign="center"
+        onLetterAnimationComplete={handleAnimationComplete}
+      />
       <h4 className={styles.subheading}>Your intelligent physical therapy assistant</h4>
       <main className={styles.main}>
         <button className={styles.buttonPrimary}>
